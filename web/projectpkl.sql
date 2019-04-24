eb86f9b9155ca388d73a9e9d5d3534c965e21882
VIRA-MUFR\USER <virananda59@gmail.com>
2019-04-24 12:29:15 +0700

    (DB) - perbaikan data

--- web/projectpkl.sql
+++ web/projectpkl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2019 at 04:27 PM
+-- Generation Time: Apr 24, 2019 at 05:11 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -39,9 +39,7 @@ CREATE TABLE `berita` (
 --
 
 INSERT INTO `berita` (`id_berita`, `JudulBerita`, `IsiBerita`) VALUES
-(1, 'Selayang Pandang', 'abupaten Malang adalah salah satu Kabupaten di Indonesia yang terletak di Propinsi Jawa Timur dan merupakan Kabupaten yang terluas kedua wilayahnya setelah Kabupaten Banyuwangi dari 38 Kabupaten/ Kota yang ada di Jawa Timur. Hal ini didukung dengan luas wilayahnya 2.977,05 km²  dan jumlah penduduk sesuai Data Pusat Statistik sebanyak 2.544.315 jiwa (tahun 2015) yang tersebar di 33 kecamatan, 378 Desa, 12 Kelurahan. Kabupaten Malang juga dikenal sebagai daerah yang kaya akan potensi diantaranya dari pertanian, perkebunan, tanaman obat keluarga dan lain sebagainya. Disamping itu juga dikenal dengan obyek-obyek wisatanya.'),
-(2, 'Sejarah', 'Ketika kerajaan Singhasari dibawah kepemimpinan Akuwu Tunggul Ametung yang beristrikan Ken Dedes, kerajaan itu dibawah kekuasaan Kerajaan Kediri. Pusat pemerintahan Singhasari saat itu berada di Tumapel. Baru setelah muncul Ken Arok yang kemudian membunuh Akuwu Tunggul Ametung dan menikahi Ken Dedes, pusat kerajaan berpindah ke Malang , setelah berhasil mengalahkan Kerajaan Kediri. Kediri saat itu jatuh ke tangan Singhasari dan turun statusnya menjadi kadipaten. Sementara Ken Arok mengangkat dirinya sebagai raja yang bergelar Prabu Kertarajasa Jayawardhana atau Dhandang Gendhis (1185 - 1222). \r\nKerajaan ini mengalami jatuh bangun. Semasa kejayaan Mataram, kerajaan-kerajaan di Malang jatuh ke tangan Mataram, seperti halnya Kerajaan Majapahit. Sementara pemerintahan pun berpindah ke Demak disertai masuknya agama Islam yang dibawa oleh Wali Songo. Malang saat itu berada di bawah pemerintahan Adipati Ronggo Tohjiwo dan hanya berstatus kadipaten. Pada masa-masa keruntuhan itu, menurut Folklore, muncul pahlawan legendaris Raden Panji Pulongjiwo. Ia tertangkap prajurit Mataram di Desa Panggungrejo yang kini disebut Kepanjen (Kepanji-an). Hancurnya kota Malang saat itu dikenal sebagai Malang Kutho Bedhah. \r\nBukti-bukti lain yang hingga sekarang merupakan saksi bisu adalah nama-nama desa seperti Kanjeron, Balandit, Turen, Polowijen, Ketindan, Ngantang dan Mandaraka. Peninggalan sejarah berupa candi-candi merupakan bukti konkrit seperti :\r\nCandi Kidal di Desa Kidal Kec. Tumpang yang dikenal sebagai tempat penyimpanan jenazah Anusapati.\r\nCandi Singhasari di Kec. Singosari sebagai penyimpanan abu jenazah Kertanegara.\r\nCandi Jago / Jajaghu di Kec. Tumpang merupakan tempat penyimpanan abu jenazah Wisnuwardhana.\r\nPada zaman VOC, Malang merupakan tempat strategis sebagai basis perlawanan seperti halnya perlawanan Trunojoyo (1674 - 1680) terhadap Mataram yang dibantu VOC. Menurut kisah, Trunojoyo tertangkap di Ngantang. Awal abad XIX ketika pemerintahan dipimpin oleh Gubernur Jenderal, Malang seperti halnya daerah-daerah di nusantara lainnya, dipimpin oleh Bupati.\r\nBupati Malang I adalah Raden Tumenggung Notodiningrat I yang diangkat oleh pemerintah Hindia Belanda berdasarkan resolusi Gubernur Jenderal 9 Mei 1820 Nomor 8 Staatblad 1819 Nomor 16. Kabupaten Malang merupakan wilayah yang strategis pada masa pemerintahan kerajaan- kerajaan. Bukti-bukti yang lain, seperti beberapa prasasti yang ditemukan menunjukkan daerah ini telah ada sejak abad VIII dalam bentuk Kerajaan Singhasari dan beberapa kerajaan kecil lainnya seperti Kerajaan Kanjuruhan seperti yang tertulis dalam Prasasti Dinoyo. Prasasti itu menyebutkan peresmian tempat suci pada hari Jum`at Legi tanggal 1 Margasirsa 682 Saka, yang bila diperhitungkan berdasarkan kalender kabisat jatuh pada tanggal 28 Nopember 760. Tanggal inilah yang dijadikan patokan hari jadi Kabupaten Malang. Sejak tahun 1984 di Pendopo Kabupaten Malang ditampilkan upacara Kerajaan Kanjuruhan, lengkap berpakaian adat zaman itu, sedangkan para hadirin dianjurkan berpakaian khas daerah Malang sebagaimana ditetapkan.'),
-(3, 'Maskot', 'Habitat jenis fauna burung Cucak Ijo ditengarai berasal dari kawasan Malang Selatan, walaupun di beberapa daerah lain juga terdapat burung sejenis. Didasari dengan latar belakang Chloropsis Sonnerati dan disusul kemudian dengan Surat Bupati Kepala Daerah Tingkat II Malang tanggal 8 Pebruari 1996 bernomor 522.4/429.024/1995 tentang pelestarian flora dan fauna, Burung Cucak Ijo dimunculkan sebagai identitas fauna Kabupaten Malang. Kemudian dikukuhkan pula dengan Surat Keputusan Bupati Kepala Daerah Tingkat II Malang, nomor 180/170/SK/429.013/1997, tentang Penetapan Maskot / Identitas Flora dan FaunaKabupaten Daerah Tingkat II Malang, tertanggal 26 April 1997. Dalam Surat Keputusan Bupati itu, untuk maskot flora ditetapkan Apel Manalagi (Malus Sylvestris Mill). Sedangkan untuk faunanya adalah Burung Cucak Ijo. Maksud penetapan maskot flora dan fauna tersebut sebagai upaya pengenalan sekaligus pelestarian yang didasari keunikan suatu jenis satwa dan tumbuhan tertentu yang terdapat di Kabupaten Malang serta merupakan ciri khas daerah. Penetapan maskot tersebut berperan pula sebagai sarana meningkatkan promosi kepariwisataan, penelitian dan pendidikan. Upaya pelestarian Burung Cucak Ijo ini dilakukan antara lain dengan cara pembangunan penangkaran terbesar yang sedang dibangun di Desa Jeru, Kecamatan Tumpang diatas lahan seluas 9,5 Ha dimana untuk Burung Cucak Ijo disediakan lahan seluas 0,5 Ha sedangkan lahan yang lain digunakan untuk pembudidayaan dan pelestarian flora dan fauna yang lain.');
+(1, 'Apa Itu Malang Smart City?', 'Ide mengenai smart city ialah kota cerdas/pintar yang inovatif menggunakan teknologi informasi dan komunikasi (TIK) yang berkelanjutan dalam membantu masyarakat mengelola sumber daya yang ada, memberikan informasi yang tepat, efisiensi operasi perkotaan, jasa, dan daya saing seiring dengan pemenuhan kebutuhan generasi saat ini dan setelahnya. Pada konsepnya, kota cerdas harus memungkinkan warga masyarakat untuk berperan secara aktif dalam pengelolaan kota dengan memberikan umpan balik kepada penyedia layanan, baik dari sektor privat maupun publik.');
 
 -- --------------------------------------------------------
 
@@ -221,7 +219,47 @@ CREATE TABLE `tb_kanpol` (
 --
 
 INSERT INTO `tb_kanpol` (`id_kanpol`, `jenis_kanpol`, `nama_kanpol`, `alamat_kanpol`, `telp_kanpol`, `lat`, `lng`, `location_status`) VALUES
-('KP001', 'Polsek', 'Resort Malang Kota Sektor Klojen', 'Jl. Kelud No.11, Kauman, Klojen, Kota Malang, Jawa Timur 65116', '(0341)361667', -7.981014, 112.625244, 1);
+('KP001', 'Polsekta', 'Kepolisian Sektor Kota Klojen', 'Jl. Kelud No.11, Kauman, Klojen, Kota Malang, Jawa Timur 65116', '(0341) 361667', -7.980768, 112.625015, 1),
+('KP002', 'Polsek', 'Kepolisian Sektor Lawang', 'Jalan Lawang View Timur I No.11 Malang, Lawang, Malang, Jawa Timur 65211', '(0341) 426624', -7.834312, 112.696907, 1),
+('KP003', 'Polsek', 'Kepolisian Sektor Singosari', 'Jalan Kertanegara, Singosari, Candirenggo, Malang, Jawa Timur 65119', '(0341) 458110', -7.890052, 112.667496, 1),
+('KP004', 'Polsek', 'Kepolisian Sektor Dau', 'Jl. Mulyo Agung No.238, Sengkaling, Mulyoagung, Dau, Malang, Jawa Timur 65151', '(0341) 460151', -7.912687, 112.584602, 1),
+('KP005', 'Polsek', 'Kepolisian Sektor Karangploso', 'Jl. Raya Kertanegara No.1, Ngambon, Girimoyo, Karangploso, Malang, Jawa Timur 65119', '(0341) 461629', -7.893248, 112.595726, 1),
+('KP006', 'Polsek', 'Kepolisian Sektor Tumpang', 'Jl. Raya Tumpang No.3, Tumpang I, Tumpang, Malang, Jawa Timur 65156', '(0341) 787310', -8.002792, 112.760040, 1),
+('KP007', 'Polsek', 'Kepolisian Sektor Pakis', 'Jl. Raya Pakis No.3, Krajan, Pakisjajar, Pakis, Malang, Jawa Timur 65154', '(0341) 791550', -7.959881, 112.717789, 1),
+('KP008', 'Polsek', 'Kepolisian Sektor Jabung', 'Jl. Letjen Sutoyo No.18, Putuk Rejo, Kemantren, Jabung, Malang, Jawa Timur 65155', '-', -7.947195, 112.744896, 1),
+('KP009', 'Polsek', 'Kepolisian Sektor Poncokusumo', 'jl. sutomo no. 52 Dsn. wates, Robyong, Wonomulyo, Poncokusumo, Malang, Jawa Timur 65157', '-', -8.041781, 112.766762, 1),
+('KP010', 'Polsek', 'Kepolisian Sektor Bululawang', 'Jl. Raya Diponegoro No.15, Bululawang, Malang, Jawa Timur 65171', '(0341) 833110', -8.078198, 112.640076, 1),
+('KP011', 'Polsek', 'Kepolisian Sektor Tajinan', 'Jl. Raya Tajinan No.22, Tajinan, Malang, Jawa Timur 65172', '(0341) 751286', -8.050075, 112.682503, 1),
+('KP012', 'Polsek', 'Kepolisian Sektor Bantur', 'Banturkrajan, Bantur, Malang, Jawa Timur 65179', '(0341) 841110', -8.311168, 112.581017, 1),
+('KP013', 'Polsek', 'Kepolisian Sektor Wajak', 'Jl. Panglima Sudirman No.90, Wajak, Malang, Jawa Timur 65173', '(0341) 823801', -8.102499, 112.730400, 1),
+('KP014', 'Polsek', 'Kepolisian Sektor Pagelaran', 'Jl. Raya Pagelaran No.46, Pagelaran, Malang, Jawa Timur 65174', '-', -8.205694, 112.613518, 1),
+('KP015', 'Polsek', 'Kepolisian Sektor Kepanjen', 'Jl. Sudirman No. 110, Ardirejo, Kepanjen, Ketawang, Ngadilangkung, Kepanjen, Malang, Jawa Timur 65163', '(0341) 399511', -8.102031, 112.581062, 1),
+('KP016', 'Polsek', 'Kepolisian Sektor Batu', 'Jl. Ahmad Yani No.11, Ngaglik, Kec. Batu, Kota Batu, Jawa Timur 65311', '(0341) 591110', -7.871288, 112.523651, 1),
+('KP017', 'Polsek', 'Kepolisian Sektor Pakisaji', 'Jl. Kolonel Slamet Supriyadi No.2, Jatirejo, Pakisaji, Malang, Jawa Timur 65162', '-', -8.064190, 112.599548, 1),
+('KP018', 'Polsek', 'Kepolisian Sektor Wagir', 'Jl. Parangargo No.97, Wagir, Parangargo, Wagir, Malang, Jawa Timur 65158', '(0341) 804663', -8.013589, 112.594765, 1),
+('KP019', 'Polsek', 'Kepolisian Sektor Sumberpucung', 'Jl. Raya Ngebruk No.114, Krajan, Ngebruk, Sumber Pucung, Malang, Jawa Timur 65165', '(0341) 385110', -8.151210, 112.514137, 1),
+('KP020', 'Polsek', 'Kepolisian Sektor Ngajum Kab. Malang', 'Ngajum, Malang, Jawa Timur 65164', '-', -8.092466, 112.540527, 1),
+('KP021', 'Polsek', 'Kepolisian Sektor Wonosari', 'Jl. Raya Bumirejo No.26, Bumireja, Kebobang, Wonosari, Malang, Jawa Timur 65164', '(0341) 370411', -8.051026, 112.502495, 1),
+('KP022', 'Polsek', 'Kepolisian Sektor Kromengan', 'Krandil, Karangrejo, Kromengan, Malang, Jawa Timur 65165', '(0341) 384300', -8.121308, 112.490341, 1),
+('KP023', 'Polsek', 'Kepolisian Sektor Pagak', 'Kulon Kali, Sumbermanjing Kulon, Pagak, Malang, Jawa Timur 65168', '(0341) 881210', -8.301713, 112.497078, 1),
+('KP024', 'Polsek', 'Kepolisian Sektor Donomulyo', 'Jl. Desa Donomulyo 911, Mulyosari, Dono Mulyo, Donomulyo, Malang, Jawa Timur 65167', '(0341) 881110', -8.285231, 112.428001, 1),
+('KP025', 'Polsek', 'Kepolisian Sektor Kalipare', 'Pitrang, Kalipare, Malang, Jawa Timur 65166', '-', -8.207325, 112.465393, 1),
+('KP026', 'Polsek', 'Kepolisian Sektor Gedangan', 'Jl. Ahmad Yani No.82, Megersari, Gedangan, Kabupaten Sidoarjo, Jawa Timur 61254', '(031) 8912221', -7.388388, 112.728165, 1),
+('KP027', 'Polsek', 'Kepolisian Sektor Turen', 'Jl. Raya Ahmad Yani No. 4, Turen, Malang, Jawa Timur 65175', '(0341) 824110', -8.170120, 112.698883, 1),
+('KP028', 'Polsek', 'Kepolisian Sektor Sumbermanjing Wetan', 'Jl. Raya Sumbermanjing Wetan, Sumbermanjing Wetan, Argotirto, Sumbermanjing, Malang, Jawa Timur 65176', '-', -8.278846, 112.690285, 1),
+('KP029', 'Polsek', 'Kepolisian Sektor Ampel Gading', 'Jl. Raya Tirtomarto, Bahroto, Tirtomarto, Ampelgading, Malang, Jawa Timur 65183', '(0341) 851110', -8.234444, 112.878769, 1),
+('KP030', 'Polsek', 'Kepolisian Sektor Dampit', 'Jl. Semeru Sel., Dampit, Malang, Jawa Timur 65181', '(0341) 896510', -8.211576, 112.748627, 1),
+('KP031', 'Polsek', 'Kepolisian Sektor Tirtoyudo', 'Jl. Desa Tirtoyudo 112, Tangsi, Tirtoyudo, Tirto Yudo, Malang, Jawa Timur 65182', '(0341) 896110', -8.230688, 112.804863, 1),
+('KP032', 'Polresta', 'Kepolisian Resor Kota Malang', 'Jl. Jaksa Agung Suprapto No.19, Samaan, Klojen, Kota Malang, Jawa Timur 65112', '(0341) 364211', -7.971952, 112.630852, 1),
+('KP033', 'Polsek', 'KKepolisian Sektor Blimbing', 'Jl. Raden Intan No.6 Malang', '(0341) 491304', -7.929858, 112.650551, 1),
+('KP034', 'Polsek', 'Kepolisian Sektor Kedungkandang', 'Jl. Raya Ki Ageng Gribig No.96, Kedungkandang, Malang, Kota Malang, Jawa Timur 65136', '(0341) 710110', -7.993411, 112.648209, 1),
+('KP035', 'Polsek', 'Kepolisian Sektor Lowokwaru', 'Gang 13 Jalan M.T. Haryono No.413, Dinoyo, Kecamatan Lowokwaru, Dinoyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65144', '(0341) 570200', -7.941406, 112.609360, 1),
+('KP036', 'Polsek', 'Kepolisian Sektor Kota Sukun', 'Jalan Kolonel Sugiono Gang 8 No.82, Ciptomulyo, Sukun, Kota Malang, Jawa Timur 65148', '(0341) 368638', -8.004249, 112.618523, 1),
+('KP037', 'Polsek', 'Kepolisian Sektor Ngantang', 'Prabon II, Kaumrejo, Ngantang, Malang, Jawa Timur 65392', '(0341) 521110', -7.856072, 112.369720, 1),
+('KP038', 'Polsek', 'Kepolisian Sektor Gondanglegi', 'Jl. Suropati No.22, Krajan, Gondanglegi Wetan, Gondanglegi, Malang, Jawa Timur 65111', '(0341) 879110', -8.176886, 112.640518, 1),
+('KP039', 'Polsek', 'Kepolisian Sektor Pujon', 'Jl. Brigjend Abd Manan Wijaya No.8, Jurangrejo, Pandesari, Pujon, Malang, Jawa Timur 65391', '(0341) 524110', -7.849765, 112.479881, 1),
+('KP040', 'Polsek', 'Kepolisian Sektor Bumiaji', 'Jl. Raya Punten, Punten, Kec. Batu, Kota Batu, Jawa Timur 65338', '(0341) 595809', -7.839242, 112.527771, 1),
+('KP041', 'Polsek', 'Kepolisian Sektor Junrejo', 'Jl. Raya Tlekung No.234, Tlekung, Kec. Batu, Kota Batu, Jawa Timur 65327', '(0341) 464111', -7.912547, 112.550140, 1);
 
 -- --------------------------------------------------------
 
@@ -286,13 +324,13 @@ CREATE TABLE `tb_rs` (
 --
 
 INSERT INTO `tb_rs` (`id_rs`, `nama_rs`, `alamat_rs`, `telp_rs`, `lat`, `lng`, `location_status`) VALUES
-('RS001', 'RSUD Balaraja', 'Jl. Rumah Sakit No.88, Tobat, Balaraja, Tangerang, Banten 15126', '021-29508382; 081211608026', -7.986563, 112.624947, 1),
-('RS002', 'RSU Selaras', 'ghhg', '021-5969145', -7.965738, 112.637886, 1),
-('RS003', 'RSIA Harapan Mulia', 'hhk', '021-5991785', -7.978025, 112.624435, 1),
-('RS004', 'RS Mulia Insani', 'kjh', '021-5962790', -7.972410, 112.631706, 1),
-('RS005', 'RSIA Tiara', 'ssss', '021-59401999', -7.973070, 112.622330, 1),
+('RS001', 'RSUD Balaraja', 'Jl. Rumah Sakit No.88, Tobat, Balaraja, Tangerang, Banten 15126', '021-29508382; 081211608026', 0.000000, 0.000000, 1),
+('RS002', 'RSU Selaras', '-', '021-5969145', 0.000000, 0.000000, 1),
+('RS003', 'RSIA Harapan Mulia', '-', '021-5991785', 0.000000, 0.000000, 1),
+('RS004', 'RS Mulia Insani', '-', '021-5962790', 0.000000, 0.000000, 1),
+('RS005', 'RSIA Tiara', '-', '021-59401999', 0.000000, 0.000000, 1),
 ('RS006', 'RS Permata Hati', 'Jl. Danau Toba Blok E 6 No.16 - 18, Lesanpuro, Kedungkandang, Kota Malang, Jawa Timur 65138', '(0341)718068', -7.980056, 112.660721, 1),
-('RS007', 'RS Suci Paramita', 'kosong', '0', -7.978080, 112.702332, 1);
+('RS007', 'RS Suci Paramita', '-', '-', 0.000000, 0.000000, 1);
 
 --
 -- Indexes for dumped tables
