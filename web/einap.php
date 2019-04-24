4510fe5f9de67dfc9c6b7888457a569fe61e6e8c
VIRA-MUFR\USER <virananda59@gmail.com>
2019-04-24 12:28:32 +0700

    (Frontend) - Einap = perbaikan link logo

--- web/einap.php
+++ web/einap.php
@@ -132,7 +132,7 @@ include"db/koneksi.php";
 <div class="wrap">
     <div class="header">
         <div class="logo">
-            <a href="index.html"><img src="images/logo2.png" alt=""/></a>
+            <a><img src="images/logo2.png" alt=""/></a>
         </div>
          <div class="log_reg">
                 <ul>
